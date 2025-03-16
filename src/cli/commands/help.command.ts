9a63a30d3dba91c59b0076a0972ba3a5d2fe0182
Ilya Lagunov <ilya.lagunov2003@gmail.com>
2025-03-16 19:28:34 +0500

    Chalk

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import chalk from 'chalk';
 
 export class HelpCommand implements Command {
   public getName(): string {
@@ -6,15 +7,21 @@ export class HelpCommand implements Command {
   }
 
   public async execute(..._parameters: string[]): Promise<void> {
+    const exampleCommand = chalk.rgb(0, 101, 255);
+    const exampleArguments = chalk.cyan;
+    const command = chalk.rgb(0, 162, 255);
+    const commandArgument = chalk.rgb(255, 120, 0);
+    const comment = chalk.magenta;
+
     console.info(`
         Программа для подготовки данных для REST API сервера.
         Пример:
-            cli.js --<command> [--arguments]
+            cli.js --<${exampleCommand('command')}> [${exampleArguments('--arguments')}]
         Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+            ${command('--version')}:                   ${comment('# выводит номер версии')}
+            ${command('--help')}:                      ${comment('# печатает этот текст')}
+            ${command('--import')} <${commandArgument('path')}>:             ${comment('# импортирует данные из TSV')}
+            ${command('--generate')} <${commandArgument('n')}> <${commandArgument('path')}> <${commandArgument('url')}>  ${comment('# генерирует произвольное количество тестовых данных')}
     `);
   }
 }
