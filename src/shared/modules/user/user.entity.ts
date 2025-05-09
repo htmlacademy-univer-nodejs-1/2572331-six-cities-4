fe0ffb71b96d0ab02e76a0a7c0bbed60e9a3c8ee
kyultex <ilya.lagunov2003@gmail.com>
2025-05-09 21:02:30 +0500

    4.17. Добавит определение модели и схемы. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import {User, UserType} from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true, minlength: 1, maxlength: 15 })
   public name: string;
+
+  @prop({ unique: true, required: true, match: /^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/ })
   public email: string;
+
+  @prop({ required: false, default: '' })
   public avatarPath: string;
+
+  @prop({ required: true, minlength: 6, maxlength: 12 })
   public password: string;
+
+  @prop({ required: true, enum: UserType })
   public userType: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
