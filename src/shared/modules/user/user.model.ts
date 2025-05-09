7ee0fc75c5bb198fdb648388ce59f4abf020a9ae
kyultex <ilya.lagunov2003@gmail.com>
2025-05-09 18:21:43 +0500

    4.12. Расширит интерфейс `UserDocument`. WIP

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,7 +1,10 @@
 import { Schema, Document, model } from 'mongoose';
 import {User, UserType} from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
   fullName: String,
@@ -9,6 +12,6 @@ const userSchema = new Schema({
   avatarPath: String,
   password: String,
   userType: UserType,
-});
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
