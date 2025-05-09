54327a62031a3d413c8b866c28678b1d453ae6d3
kyultex <ilya.lagunov2003@gmail.com>
2025-05-09 20:38:28 +0500

    4.14. Задействует встроенную валидацию. WIP

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -7,14 +7,34 @@ export interface UserDocument extends User, Document {
 }
 
 const userSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: [1, 'Min length for avatar path is 1'],
+    maxlength: [15, 'Max length for avatar path is 15'],
+  },
   email: {
     type: String,
     unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
   },
-  avatarPath: String,
-  password: String,
-  userType: UserType,
+  avatarPath: {
+    type: String,
+    required: false,
+    match: [/^.+(\.jpg|\.png)$/i, 'Avatar path is incorrect'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Min length for avatar path is 6'],
+    maxlength: [12, 'Max length for avatar path is 12'],
+  },
+  userType: {
+    type: String,
+    required: true,
+    enum: Object.values(UserType),
+  },
 }, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
